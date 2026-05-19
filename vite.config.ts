785ec38a58d2b4ed2957a288f346db1803fec647
d0kuzu <dkobdabaev@mail.ru>
2026-05-19 19:52:58 +0500

    chore: configure docker, nginx, and set base path to /frontend/

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/alau-cloud-ai/" : "/",
+  base: mode === "production" ? "/frontend/" : "/",
   server: {
     host: "::",
     port: 8080,
